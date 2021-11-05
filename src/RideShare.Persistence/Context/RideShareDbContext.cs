aa9c65d84ab4ee7fd7b4dc191d858f09e60d42cd
tolgacakir <cakrtolga13@gmail.com>
2021-11-05 22:16:06 +0300

    add RideShareDbContext

---
+++ src/RideShare.Persistence/Context/RideShareDbContext.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using RideShare.Application.Common;
+using RideShare.Domain.Common;
+using RideShare.Domain.Entities;
+using RideShare.Persistence.Configurations;
+
+namespace RideShare.Persistence.Context
+{
+    public class RideShareDbContext : DbContext, IRideShareDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<TravelPlan> TravelPlans { get; set; }
+        public DbSet<TravelDemand> TravelDemands { get; set; }
+
+        public RideShareDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedAt = DateTime.Now;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return await  base.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await Database.BeginTransactionAsync();
+        }
+    }
+}
