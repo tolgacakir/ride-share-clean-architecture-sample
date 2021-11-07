66422c9c463a2b48925c179692acd04961a8a3c9
tolgacakir <cakrtolga13@gmail.com>
2021-11-08 00:19:20 +0300

    add GetPassengerByTravelPlanQueryHandler

---
+++ src/RideShare.Application/Queries/Passengers/GetPassengersByTravelPlan/GetPassengersByTravelPlanQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using RideShare.Domain.Entities;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using RideShare.Application.Common;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace RideShare.Application.Queries.Passengers.GetPassengersByTravelPlan
+{
+    public class GetPassengersByTravelPlanQueryHandler : IRequestHandler<GetPassengersByTravelPlanRequest, List<GetPassengersByTravelPlanResponse>>
+    {
+        private readonly IRideShareDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPassengersByTravelPlanQueryHandler(IRideShareDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetPassengersByTravelPlanResponse>> Handle(GetPassengersByTravelPlanRequest request, CancellationToken cancellationToken)
+        {
+            var plan = await _context.TravelPlans
+                .Where(x => x.Id == request.TravelPlanId)
+                .Include(x => x.Demands)
+                .ThenInclude(x => x.Passenger)
+                .FirstOrDefaultAsync();
+            var passengers = plan.Passengers;
+
+            return _mapper.Map<List<GetPassengersByTravelPlanResponse>>(passengers);
+        }
+    }
+
+    public class GetPassengersByTravelPlanRequest : IRequest<List<GetPassengersByTravelPlanResponse>>
+    {
+        public int TravelPlanId { get; set; }
+    }
+
+    public class GetPassengersByTravelPlanResponse
+    {
+        public string PassengerName { get; set; }
+    }
+}
