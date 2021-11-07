bea1ab43edf6c25724edc76762c1082276857492
tolgacakir <cakrtolga13@gmail.com>
2021-11-07 23:33:35 +0300

    add UpdateUserPasswordCommandHandler

---
+++ src/RideShare.Application/Commands/Users/UpdateUserPassword/UpdateUserPassword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RideShare.Application.Common;
+using RideShare.Domain.Entities;
+
+namespace RideShare.Application.Commands.Users.UpdateUserPassword
+{
+    public class UpdateUserPasswordCommandHandler : IRequestHandler<UpdateUserPasswordRequest, UpdateUserPasswordResponse>
+    {
+        private readonly IRideShareDbContext _context;
+
+        public UpdateUserPasswordCommandHandler(IRideShareDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UpdateUserPasswordResponse> Handle(UpdateUserPasswordRequest request, CancellationToken cancellationToken)
+        {
+            var user = _context.Users.Where(x => x.Id == request.UserId)
+                .FirstOrDefaultAsync().Result;
+
+            user.SetPassword(request.Password);
+
+            var result = await _context.SaveChangesAsync(cancellationToken);
+
+            return new UpdateUserPasswordResponse
+            {
+                Result = result
+            };
+        }
+    }
+
+    public class UpdateUserPasswordRequest : IRequest<UpdateUserPasswordResponse>
+    {
+        public Guid UserId { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UpdateUserPasswordResponse
+    {
+        public int Result { get; set; }
+    }
+}
