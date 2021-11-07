086a037b4d4ef481a44c16055e5fae7a820d5d1e
tolgacakir <cakrtolga13@gmail.com>
2021-11-07 23:32:56 +0300

    add CreateTravelDemandCommandHandler

---
+++ src/RideShare.Application/Commands/TravelDemands/CreateTravelDemand/CreateTravelDemandCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+using RideShare.Application.Common;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace RideShare.Application.Commands.TravelDemands.CreateTravelDemand
+{
+    public class CreateTravelDemandCommandHandler : IRequestHandler<CreateTravelDemandRequest, CreateTravelDemandResponse>
+    {
+        private readonly IRideShareDbContext _context;
+
+        public CreateTravelDemandCommandHandler(IRideShareDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CreateTravelDemandResponse> Handle(CreateTravelDemandRequest request, CancellationToken cancellationToken)
+        {
+            var passenger = await _context.Passengers.Where(x => x.Id == request.PassengerId)
+                .FirstOrDefaultAsync();
+
+            var plan = await _context.TravelPlans.Where(x => x.Id == request.TravelPlanId)
+                .FirstOrDefaultAsync();
+
+            var demand = passenger.CreateTravelDemand(plan);
+
+
+            _context.TravelDemands.Add(demand);
+            var result = await _context.SaveChangesAsync(cancellationToken);
+            return new CreateTravelDemandResponse
+            {
+                Result = result
+            };
+        }
+    }
+
+    public class CreateTravelDemandRequest : IRequest<CreateTravelDemandResponse>
+    {
+        public int PassengerId { get; set; }
+        public int TravelPlanId { get; set; }
+    }
+
+    public class CreateTravelDemandResponse
+    {
+        public int Result { get; set; }
+    }
+}
