6d3e8a72c93294031b1118e1e92dd1641f19db11
tolgacakir <cakrtolga13@gmail.com>
2021-11-07 22:04:41 +0300

    add FinishTravelPlanCommandHandler

---
+++ src/RideShare.Application/Commands/TravelPlans/FinishTravelPlan/FinishTravelPlanCommandHandler.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RideShare.Application.Common;
+
+namespace RideShare.Application.Commands.TravelPlans.FinishTravelPlan
+{
+    public class FinishTravelPlanCommandHandler : IRequestHandler<FinishTravelPlanRequest, FinishTravelPlanResponse>
+    {
+        private readonly IRideShareDbContext _context;
+        public async Task<FinishTravelPlanResponse> Handle(FinishTravelPlanRequest request, CancellationToken cancellationToken)
+        {
+            var driver = await _context.Drivers.Include(x => x.TravelPlans)
+                .Where(x=>x.Id == request.DriverId)
+                .FirstOrDefaultAsync();
+
+            var plan = driver.AllTravelPlans.Where(x=>x.Id == request.TravelPlanId).FirstOrDefault();
+            driver.FinishTravelPlan(plan);
+
+            var result = await _context.SaveChangesAsync(cancellationToken);
+
+            return new FinishTravelPlanResponse{
+                Result = result
+            };
+        }
+    }
+
+    public class FinishTravelPlanRequest : IRequest<FinishTravelPlanResponse> 
+    {
+        public int TravelPlanId { get; set; }
+        public int DriverId { get; set; }
+    }
+
+    public class FinishTravelPlanResponse
+    {
+        public int Result { get; set; }
+    }
+}
