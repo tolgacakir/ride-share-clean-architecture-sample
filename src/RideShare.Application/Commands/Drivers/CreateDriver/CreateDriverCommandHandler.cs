f88c6c64f1c882ea1981de5c714c548eb91cb67c
tolgacakir <cakrtolga13@gmail.com>
2021-11-07 22:02:49 +0300

    add CreateDriverCommandHandler

---
+++ src/RideShare.Application/Commands/Drivers/CreateDriver/CreateDriverCommandHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RideShare.Application.Common;
+using RideShare.Domain.Entities;
+
+namespace RideShare.Application.Commands.Drivers.CreateDriver
+{
+    public class CreateDriverCommandHandler : IRequestHandler<CreateDriverRequest, CreateDriverResponse>
+    {
+        private readonly IRideShareDbContext _context;
+
+        public CreateDriverCommandHandler(IRideShareDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CreateDriverResponse> Handle(CreateDriverRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .Include(x=> x.Driver)
+                .FirstOrDefaultAsync(x => x.Id == request.UserId);
+            if (user.Driver != null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var driver = user.CreateDriver();
+            _context.Drivers.Add(driver);
+            var result = await _context.SaveChangesAsync(cancellationToken);
+
+            return new CreateDriverResponse{
+                Result = result
+            };
+        }
+    }
+
+    public class CreateDriverRequest : IRequest<CreateDriverResponse>
+    {
+        public Guid UserId { get; set; }
+    }
+
+    public class CreateDriverResponse
+    {
+        public int Result { get; set; }
+    }
+}
