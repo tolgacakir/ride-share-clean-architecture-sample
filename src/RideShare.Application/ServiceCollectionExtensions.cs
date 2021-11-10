b1d3f95e9dfcdf42be5658d94caa6d362bf43b64
tolgacakir <cakrtolga13@gmail.com>
2021-11-10 21:24:09 +0300

    add ServiceCollectionsExtensions of application

---
+++ src/RideShare.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideShare.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
