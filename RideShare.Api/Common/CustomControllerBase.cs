aeb9ceaefd992f4b2f60ab19805546f31d3edb23
tolgacakir <cakrtolga13@gmail.com>
2021-11-10 22:12:21 +0300

    add CustomControllerBase

---
+++ RideShare.Api/Common/CustomControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Api.Common
+{
+    public abstract class CustomControllerBase : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+
+        protected CustomControllerBase(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
