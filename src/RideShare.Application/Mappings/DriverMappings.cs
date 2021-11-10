71fc99328913beb57c94715aa21e479c3a25d1ad
tolgacakir <cakrtolga13@gmail.com>
2021-11-10 19:30:52 +0300

    add DriverMappings

---
+++ src/RideShare.Application/Mappings/DriverMappings.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace RideShare.Application.Mappings
+{
+    public class DriverMappings : Profile
+    {
+        public DriverMappings()
+        {
+            
+        }
+    }
+}
