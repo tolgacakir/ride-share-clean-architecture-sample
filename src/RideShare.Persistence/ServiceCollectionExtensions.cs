34e05c4e362d0fd7f2fd2e441e0f466cd1cdd20b
tolgacakir <cakrtolga13@gmail.com>
2021-11-10 21:23:53 +0300

    add ServiceCollectionsExtensions of persistence

---
+++ src/RideShare.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RideShare.Persistence.Context;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RideShare.Application.Common;
+
+namespace RideShare.Persistence
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RideShareDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IRideShareDbContext>(provider => provider.GetService<RideShareDbContext>());
+        }
+    }
+}
