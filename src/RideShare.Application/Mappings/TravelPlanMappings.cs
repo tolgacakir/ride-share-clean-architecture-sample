d486e7235c506f0c20d82cd6a35f80e4d762ee4b
tolgacakir <cakrtolga13@gmail.com>
2021-11-10 19:31:36 +0300

    add TravelPlanMappings

---
+++ src/RideShare.Application/Mappings/TravelPlanMappings.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using AutoMapper;
+using RideShare.Application.Queries.TravelPlans.GetActiveTravelPlans;
+using RideShare.Application.Queries.TravelPlans.GetTravelPlansByPassenger;
+using RideShare.Domain.Entities;
+
+namespace RideShare.Application.Mappings
+{
+    public class TravelPlanMappings : Profile
+    {
+        public TravelPlanMappings()
+        {
+            CreateMap<TravelPlan, GetActiveTravelPlansResponse>()
+                .ForMember(res => res.DriverName, t => t.MapFrom(x=>x.Driver.User.Username));
+
+            CreateMap<TravelPlan, GetTravelPlansByPassengerResponse>()
+                .ForMember(res => res.PassengerNames, t => t.MapFrom(x=>x.Passengers.Select(p => p.User.Username)))
+                .ForMember(res => res.DriverName, t => t.MapFrom(x => x.Driver.User.Username));
+        }
+    }
+}
