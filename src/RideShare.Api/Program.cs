52d9500ea575a39e78ababd40ea3a64eaf162fc5
Tolga Cakir <7142432+tolgacakir@users.noreply.github.com>
2021-11-11 17:32:35 +0300

    update Program.cs

--- src/RideShare.Api/Program.cs
+++ src/RideShare.Api/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RideShare.Persistence.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,16 @@ namespace RideShare.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var dbContext = services.GetRequiredService<RideShareDbContext>();
+            if (dbContext.Database.IsSqlServer())
+            {
+                dbContext.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
