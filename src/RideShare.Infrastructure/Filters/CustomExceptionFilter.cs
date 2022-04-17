825ca7e779e5c7fc93d3da8c1ac2d9d3f3c54434
tolgacakir <cakrtolga13@gmail.com>
2022-04-17 21:24:29 +0300

    update CustomExceptionFilter

--- src/RideShare.Infrastructure/Filters/CustomExceptionFilter.cs
+++ src/RideShare.Infrastructure/Filters/CustomExceptionFilter.cs
@@ -4,22 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Threading.Tasks;
 using System;
+using FluentValidation;
 
 namespace RideShare.Infrastructure.Filters
 {
     public class CustomExceptionFilter :ExceptionFilterAttribute
-    {
+    { 
         public async override Task OnExceptionAsync(ExceptionContext context)
         {
-            int statusCode = (int)HttpStatusCode.InternalServerError;
-            if (context.Exception is NullReferenceException)
+            int statusCode;
+            
+            if (context.Exception is NullReferenceException or ArgumentNullException)
+            {
                 statusCode = (int)HttpStatusCode.NotFound;
+            }
+            else if (context.Exception is ArgumentException or ArgumentOutOfRangeException or ValidationException or InvalidOperationException)
+            {
+                statusCode = (int)HttpStatusCode.BadRequest;   
+            }
+            else
+            {
+                statusCode = (int)HttpStatusCode.InternalServerError;
+            }
 
-            context.HttpContext.Response.ContentType = "application/json";
             context.HttpContext.Response.StatusCode = statusCode;
+            context.HttpContext.Response.ContentType = "application/json";
+            
             context.Result = new ObjectResult(new {
-                errors = new[] { context.Exception.Message },
-                source = context.Exception.Source
+                Errors = new[] { context.Exception.Message },
+                Source = context.Exception.Source,
             });
 
             await Task.CompletedTask;
